380ad2e1f03cc62e251c2e5790cf048e06f4e9b3
Sairam Harikrishnan <sai_hari98@hotmail.com>
2018-03-02 15:26:34 +0530

    Commit 4

--- app/src/main/java/com/example/sai_h/reviewuoso/MainActivity.java
+++ app/src/main/java/com/example/sai_h/reviewuoso/MainActivity.java
@@ -2,41 +2,34 @@ package com.example.sai_h.reviewuoso;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.concurrent.ExecutionException;
-
-import javax.net.ssl.HttpsURLConnection;
-
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,com.google.android.gms.location.LocationListener {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,com.google.android.gms.location.LocationListener,AdapterView.OnItemSelectedListener{
     double lat, lon;
     Location loc;
     GoogleApiClient mGoogleApiClient;
     LocationRequest mRequest;
-    EditText e;
-    HttpsGetter h = new HttpsGetter();
+    Spinner sp;
+    JSONObject json;
+    String[] category = new String[]{"atm","bakery","bank","bar","beauty_salon","book_store","cafe","clothing_store","department_store","electronics_store","gas_station","hospital","library","pharmacy","restaurant","school","shoe_store","shopping_mall","supermarket"};
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +37,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
         buildGoogleapiclient();
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        sp = (Spinner)findViewById(R.id.spin);
+        sp.setOnItemSelectedListener(this);
     }
 
     synchronized void buildGoogleapiclient() {
@@ -87,14 +82,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         if (loc != null) {
             lat = loc.getLatitude();
             lon = loc.getLongitude();
-            String s = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+String.valueOf(lat)+","+String.valueOf(lon)+"&radius=5000&type=restaurant&key=AIzaSyBx77XwaQLYdMOqqlb3n3x6-talZWLhyjk";
-            try {
-                result(new HttpsGetter().execute(s).get());
-            } catch (InterruptedException e1) {
-                e1.printStackTrace();
-            } catch (ExecutionException e1) {
-                e1.printStackTrace();
-            }
         }
     }
 
@@ -121,7 +108,28 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     }
     public void result(String s){
         TextView t = (TextView)findViewById(R.id.result);
+        System.out.println(s);
         t.setText(s);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        int i1 = sp.getSelectedItemPosition();
+        Log.i("Pos",String.valueOf(i));
+        String cat = category[i1];
+        String s = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+String.valueOf(lat)+","+String.valueOf(lon)+"&radius=5000&type="+cat+"&key=AIzaSyBx77XwaQLYdMOqqlb3n3x6-talZWLhyjk";
+        try {
+            result(new HttpsGetter().execute(s).get());
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        } catch (ExecutionException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
 
