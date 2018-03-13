8456ffe69cac73c626219b5c838cf9c82974e35d
Sairam Harikrishnan <sai_hari98@hotmail.com>
2018-03-13 14:33:25 +0530

    Commit 8

--- app/src/main/java/com/example/sai_h/reviewuoso/MainActivity.java
+++ app/src/main/java/com/example/sai_h/reviewuoso/MainActivity.java
@@ -1,22 +1,28 @@
 package com.example.sai_h.reviewuoso;
 import android.Manifest;
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
@@ -46,11 +52,17 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     ViewPager vp;
     PagerAdapter pa;
     TabLayout tl;
+    DrawerLayout d;
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        android.support.v7.widget.Toolbar t = findViewById(R.id.my);
-        setSupportActionBar(t);
+        android.support.v7.widget.Toolbar t1 = findViewById(R.id.my);
+        setSupportActionBar(t1);
+        android.support.v7.app.ActionBar a = getSupportActionBar();
+        a.setDisplayHomeAsUpEnabled(true);
+        a.setHomeAsUpIndicator(R.drawable.ic_menu);
+        d = findViewById(R.id.drawer);
         ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
         buildGoogleapiclient();
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
@@ -63,6 +75,15 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             TabLayout.Tab t = tl.getTabAt(i);
             t.setCustomView(pa.getTabView(i));
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.home: d.openDrawer(GravityCompat.START);
+                            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     synchronized void buildGoogleapiclient() {
